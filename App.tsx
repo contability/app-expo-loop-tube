91c5d2b0bf18774c63736d6c3c25bf63ad8db2c8
furium <shouldBnewB@gmail.com>
2025-08-04 16:17:54 +0900

    (refactor) 비디오 플레이어 관련 함수 제거 및 코드 정리

--- App.tsx
+++ App.tsx
@@ -101,24 +101,9 @@ const App = () => {
             });
           }
 
-          // 4. The API will call this function when the video player is ready.
-          function onPlayerReady(event) {
-            event.target.playVideo();
-          }
+          function onPlayerReady(event) {}
 
-          // 5. The API calls this function when the player's state changes.
-          //    The function indicates that when playing a video (state=1),
-          //    the player should play for six seconds and then stop.
-          var done = false;
-          function onPlayerStateChange(event) {
-            if (event.data == YT.PlayerState.PLAYING && !done) {
-              setTimeout(stopVideo, 6000);
-              done = true;
-            }
-          }
-          function stopVideo() {
-            player.stopVideo();
-          }
+          function onPlayerStateChange(event) {}
         </script>
       </body>
     </html>`;
